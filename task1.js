0e0f175c61eb49b98c3fac6cf56918266033a654
spachaiyappan47 <141689075+spachaiyappan47@users.noreply.github.com>
2023-09-18 14:17:05 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,5 +1,15 @@
-const { isDeepStrictEqual } = require("util")
-
+// compare to Object
 let obj1={name:"person 1",age:5}
 let obj2={age:5,name:"person 1"}
-console.log(isDeepStrictEqual(obj1,obj2));
+
+function Equal(first,second){
+    for( let i in second){
+        if(first[i]===second[i]){
+            return true
+        }
+        
+    }
+    return false
+};
+
+console.log(Equal(obj1,obj2))
